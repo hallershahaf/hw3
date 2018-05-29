b6e1e3d081806b2c58591ef771174ac980a26df0
Shay Agroskin <agrosshay@gmail.com>
2018-05-29 20:08:45 +0300

    Fixed some compilation errors

--- dflow_calc.c
+++ dflow_calc.c
@@ -3,6 +3,7 @@
 
 #include "dflow_calc.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 // Defines
 
@@ -29,14 +30,19 @@ typedef struct {
 ProgData* InitProg(unsigned int numOfInsts);
 void CheckDep(ProgData *data, InstInfo progTrace[], int cmd_num, int dep_num,
 	unsigned int opsLatency[]);
-unsigned int findProgDepth(ProgData *data, unsigned int opsLatency[]);
+unsigned int findProgDepth(ProgData *data, unsigned int opsLatency[], InstInfo progTrace[]);
+
+
+int max(int a, int b) {
+	return (a>b) ? a : b;
+}
 
 // Actual Code
 
 ProgData* InitProg(unsigned int numOfInsts) {
 
 	// Allocation
-	ProgData *data = malloc(sizeof(ProgData)));
+	ProgData *data = malloc(sizeof(ProgData));
 	if (!data)
 		return PROG_CTX_NULL;
 
@@ -48,7 +54,7 @@ ProgData* InitProg(unsigned int numOfInsts) {
 
 	// Init phase
 	data->arr_len = numOfInsts;
-	for (int i = 0; , < arr_len; i++) {
+	for (int i = 0; i < numOfInsts; i++) {
 		(data->inst_array[i]).is_last_dep = true;
 		(data->inst_array[i]).dep1 = NO_DEP;
 		(data->inst_array[i]).dep2 = NO_DEP;
@@ -64,15 +70,17 @@ void CheckDep(ProgData *data, InstInfo progTrace[], int cmd_num, int dep_num,
 	bool is_updated = false;
 	InstInfo cmd = progTrace[cmd_num];
 	InstInfo dep_cmd = progTrace[dep_num];
+	int dep_depth;
+	int cmd_depth;
 
 	// Update dep1  (Src1)
-	if ((cmd.src1Idx != NOT_USED && cmd.src1Idx == dep_cmd.dstIdx) {
+	if (cmd.src1Idx != NOT_USED && cmd.src1Idx == dep_cmd.dstIdx) {
 		if (data->inst_array[cmd_num].dep1 < dep_num)
 			data->inst_array[cmd_num].dep1 = dep_num;
 		is_updated = true;
 	}
 	// Update dep2  (Src2)
-	if ((cmd.src2Idx != NOT_USED && cmd.src2Idx == dep_cmd.dstIdx) {
+	if (cmd.src2Idx != NOT_USED && cmd.src2Idx == dep_cmd.dstIdx) {
 		if (data->inst_array[cmd_num].dep1 < dep_num)
 			data->inst_array[cmd_num].dep1 = dep_num;
 		is_updated = true;
@@ -83,14 +91,13 @@ void CheckDep(ProgData *data, InstInfo progTrace[], int cmd_num, int dep_num,
 
 updated:
 	// Update the dep command, that it is depended upon.
-	data->inst_array[dep_cmd].is_last_dep = false;
+	data->inst_array[dep_num].is_last_dep = false;
 	
 	// Update depth
-	int dep_depth = data->inst_array[dep_num].depth;
-	int cmd_depth = data->inst_array[cmd_num].depth;
+	dep_depth = data->inst_array[dep_num].depth;
+	cmd_depth = data->inst_array[cmd_num].depth;
 	data->inst_array[cmd_num].depth = 
 		max(dep_depth + opsLatency[dep_cmd.opcode], cmd_depth);
-	return;
 }
 
 unsigned int findProgDepth(ProgData *data, unsigned int opsLatency[], InstInfo progTrace[]) {
@@ -111,7 +118,10 @@ ProgCtx analyzeProg(const unsigned int opsLatency[],  InstInfo progTrace[], unsi
 		return PROG_CTX_NULL;
 
 	// The Fun Stuff
-
+/*
+ *CheckDep(ProgData *data, InstInfo progTrace[], int cmd_num, int dep_num,
+ *                unsigned int opsLatency[])
+ */
 	// We start from 1 because cmd no.0 has no dependencies, with a depth of 0.
 	for (int i = 1; i < numOfInsts; i++) {
 		CheckDep(data, progTrace, i, i-1);
